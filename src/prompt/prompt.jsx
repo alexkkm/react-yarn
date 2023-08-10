9501c6e9e35b55ed46838cd0e659cd2f81a291a1
alexkkm <alexkong0222@gmail.com>
2023-08-10 17:55:44 +0800

    replaced compile function with eval()

--- src/prompt/prompt.jsx
+++ src/prompt/prompt.jsx
@@ -1,7 +1,6 @@
 // External package and dependency
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 // Components from for prompt
@@ -29,6 +28,8 @@ const javascriptDefault = `/**
 */
 
 // Time: O(log n)
+
+/*
 const binarySearch = (arr, target) => {
  return binarySearchHelper(arr, target, 0, arr.length - 1);
 };
@@ -52,6 +53,12 @@ const binarySearchHelper = (arr, target, start, end) => {
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const target = 5;
 console.log(binarySearch(arr, target));
+*/
+
+function main(){
+  alert("Press F12 to open console");
+}
+main();
 `;
 
 // The main component
@@ -90,88 +97,20 @@ const Prompt = () => {
     }
   };
 
-  // Function of compling the source code
-  const handleCompile = () => {
-    setProcessing(true);
-    const formData = {
-      language_id: language.id,
-      // encode source code in base64
-      source_code: btoa(code),
-      stdin: btoa(customInput),
-    };
-    const options = {
-      method: "POST",
-      url: process.env.REACT_APP_RAPID_API_URL,
-      params: { base64_encoded: "true", fields: "*" },
-      headers: {
-        "content-type": "application/json",
-        "Content-Type": "application/json",
-        "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
-        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-      },
-      data: formData,
-    };
 
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log("res.data", response.data);
-        const token = response.data.token;
-        checkStatus(token);
-      })
-      .catch((err) => {
-        let error = err.response ? err.response.data : err;
-        // get error status
-        let status = err.response.status;
-        console.log("status", status);
-        if (status === 429) {
-          console.log("too many requests", status);
-
-          showErrorToast(
-            `Quota of 100 requests exceeded for the Day! Please read the blog on freeCodeCamp to learn how to setup your own RAPID API Judge0!`,
-            10000
-          );
-        }
-        setProcessing(false);
-        console.log("catch block...", error);
-      });
-  };
-
-  const checkStatus = async (token) => {
-    const options = {
-      method: "GET",
-      url: process.env.REACT_APP_RAPID_API_URL + "/" + token,
-      params: { base64_encoded: "true", fields: "*" },
-      headers: {
-        "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
-        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-      },
-    };
+  // Self define compile
+  const executeCode = () => {
+    var output=document.getElementById("output")
     try {
-      let response = await axios.request(options);
-      let statusId = response.data.status?.id;
-
-      // Processed - we have a result
-      if (statusId === 1 || statusId === 2) {
-        // still processing
-        setTimeout(() => {
-          checkStatus(token);
-        }, 2000);
-        return;
-      } else {
-        setProcessing(false);
-        setOutputDetails(response.data);
-        showSuccessToast(`Compiled Successfully!`);
-        console.log("response.data", response.data);
-        return;
-      }
-    } catch (err) {
-      console.log("err", err);
-      setProcessing(false);
-      showErrorToast();
+      // You can execute the code using eval() function, but be cautious as it can be a security risk.
+      var result=eval(code);
+      output.textContent=result; 
+    } catch (error) {
+      output.textContent="Error: "+error.message;
     }
   };
 
+  // Changing theme from ThemeDropdown
   function handleThemeChange(th) {
     const theme = th;
     console.log("theme...", theme);
@@ -187,29 +126,6 @@ const Prompt = () => {
       setTheme({ value: "oceanic-next", label: "Oceanic Next" })
     );
   }, []);
-
-  const showSuccessToast = (msg) => {
-    toast.success(msg || `Compiled Successfully!`, {
-      position: "top-right",
-      autoClose: 1000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
-  };
-  const showErrorToast = (msg, timer) => {
-    toast.error(msg || `Something went wrong! Please try again.`, {
-      position: "top-right",
-      autoClose: timer ? timer : 1000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
-  };
 
 
   return (
@@ -256,7 +172,8 @@ const Prompt = () => {
               setCustomInput={setCustomInput}
             />
             <button
-              onClick={handleCompile}
+              onClick={
+                executeCode}
               disabled={!code}
               className={classnames(
                 "mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0",
@@ -267,6 +184,7 @@ const Prompt = () => {
             </button>
           </div>
           {outputDetails && <OutputDetails outputDetails={outputDetails} />}
+          <pre id="output"></pre>
         </div>
       </div>
       <Footer />

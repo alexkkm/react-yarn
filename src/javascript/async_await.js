7763ff692f5c81129cee8c0b251abcb23d869a6a
alexkkm <alexkong0222@gmail.com>
2023-08-30 12:54:48 +0800

    updated async folder code

--- src/javascript/async_await.js
+++ src/javascript/async_await.js
@@ -1,15 +1,4 @@
-async function testing(){
-
-
-    console.log("Start!");
-    console.log("Ans1 generating");
-    const ans1=await analysis(2);
-    console.log("Ans1 finished");
-    console.log(ans1);
-    const ans2=await analysis(ans1);
-    console.log(ans2);
-}
-
+// function that return a promise, is in attribute of "promise"
 function analysis(num){
     return new Promise(resolver=>{
         setTimeout(()=>{
@@ -18,4 +7,21 @@ function analysis(num){
 })
 }
 
-testing()
+// Example of using await for promise in a async function
+async function awaitForPromise(){   // if the function inside include componenet of "await", then it should define "async"
+    console.log("Start!");
+    console.log("Ans1 generating");
+    const ans1=await analysis(2);   // use "await" to pause the code below,  until the "promise" is executed
+    console.log("Ans1 finished");
+    console.log(ans1);
+    console.log("Ans2 generating");
+    const ans2=await analysis(ans1);
+    console.log("Ans2 finished");
+    console.log(ans2);
+}
+
+async function main(){  
+    await awaitForPromise();
+    console.log("End of main");
+}
+main();

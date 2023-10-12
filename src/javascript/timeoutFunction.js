90f317d911235faf869230da281089d957ecf71a
alexkkm <alexkong0222@gmail.com>
2023-10-12 16:08:46 +0800

    update with timeout()

--- src/javascript/timeoutFunction.js
+++ src/javascript/timeoutFunction.js
@@ -1,16 +1,33 @@
-function TimeoutFuntion() {
+// Timeout function
+const timeout = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function example() {
+    console.log("Start")
+    await timeout(1000)
+    console.log("Stop")
+}
+
+example();
+
+/**************************************************************************************/
+
+
+// Example of the usage of setTimeout()
+function setTimeoutExample() {
     console.log("start!");
-    
-    setTimeout(First, 1000);   // Will do the First() 1 sec after MainFunction runned
+
+    setTimeout(First, 1000);   // Will do the First() 1 sec after MainFunction run
 
     function First() {
         console.log("First");
-        setTimeout(Second, 4000);   // Will do the Second() 4 sec after First() runned
+        setTimeout(Second, 4000);   // Will do the Second() 4 sec after First() run
     }
 
     function Second() {
         console.log("Second");
-        setTimeout(Third, 3000);   // Will do the Third() 3 sec after Second() runned
+        setTimeout(Third, 3000);   // Will do the Third() 3 sec after Second() run
     }
 
     function Third() {
@@ -20,4 +37,3 @@ function TimeoutFuntion() {
     console.log("End");
 }
 
-TimeoutFuntion();
